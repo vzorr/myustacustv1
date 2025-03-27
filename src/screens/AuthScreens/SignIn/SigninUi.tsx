4841678f3835b58407a3576e894823af2bb71bc0
Abid-Hussain786-coder <abidghouri173@gmail.com>
2025-03-27 23:29:35 +0500

    new pull

--- src/screens/AuthScreens/SignIn/SigninUi.tsx
+++ src/screens/AuthScreens/SignIn/SigninUi.tsx
@@ -24,10 +24,6 @@ const SignInScreen: React.FC<UserNavigationRootProps<"SignIn">> = (props) => {
     const [isLogin, setIsLogin] = useState(isSignIn)
 
     const handleScreen = () => {
-<<<<<<< HEAD
-        console.log("isLogin", isLogin)
-=======
->>>>>>> b965f97f5b292d5a15d63222f22fe369ffb4310d
         setIsLogin(!isLogin)
     }
     useEffect(() => {
@@ -84,28 +80,8 @@ const SignInScreen: React.FC<UserNavigationRootProps<"SignIn">> = (props) => {
             validationSchema={isLogin ? logInSchema : SigUpSchema}
 
         >
-<<<<<<< HEAD
-            {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-                <Animated.View style={[styles.container, { opacity: screenOpacity }]}>
-                    <AuthOverlay color={COLORS.authBgColor} />
-                    <Animated.View style={[styles.logoContainer, { transform: logoTransform }]}>
-                        <SVGIcons.MyUstaLogo />
-                    </Animated.View>
-                    <Animated.View style={[styles.content, { transform: [{ translateX: contentAnim }] }]}>
-                        <Text style={styles.title}>{isLogin ? "Sign in to your account." : "Create your account."}</Text>
-                        <CustomTextInput
-                            placeholder={isLogin ? "Email" : "Email or phone number"}
-                            placeholderTextColor={COLORS.white}
-                            value={values?.email}
-                            onChangeText={handleChange('email')}
-                            onBlur={handleBlur("email")}
-                        />
-                        {errors?.email && touched?.email &&
-                            <ErrorText
-                                error={errors.email}
-=======
-            {({ handleChange, handleBlur, handleSubmit, values, errors, touched , resetForm}) => {
-                 useEffect(() => {
+            {({ handleChange, handleBlur, handleSubmit, values, errors, touched, resetForm }) => {
+                useEffect(() => {
                     resetForm();
                 }, [isLogin]);
                 return (
@@ -122,7 +98,6 @@ const SignInScreen: React.FC<UserNavigationRootProps<"SignIn">> = (props) => {
                                 value={values?.emailOrPhone}
                                 onChangeText={handleChange('emailOrPhone')}
                                 onBlur={handleBlur("emailOrPhone")}
->>>>>>> b965f97f5b292d5a15d63222f22fe369ffb4310d
                             />
                             {errors?.emailOrPhone && touched?.emailOrPhone &&
                                 <ErrorText
@@ -139,42 +114,6 @@ const SignInScreen: React.FC<UserNavigationRootProps<"SignIn">> = (props) => {
                                         onBlur={handleBlur("password")}
                                         value={values?.password}
                                     />
-<<<<<<< HEAD
-                                }
-                                <Text
-                                    onPress={handleForgotPassword}
-                                    style={styles.forgotPassword}>Forgot Password?</Text>
-                            </>
-                        }
-                        <CustomButton
-                            title={isLogin ? "Sign In" : "Create Account"}
-                            onPress={handleSubmit}
-                            style={styles.signInButton}
-                        />
-                        <OrDivider />
-                        <SocialLogin
-                            title="Sign in with Google"
-                            style={styles.socialButton}
-                            textStyle={styles.socialButtonText}
-                            loginType='google'
-                        />
-                        <SocialLogin
-                            title="Sign in with Apple"
-                            style={styles.socialButton}
-                            textStyle={styles.socialButtonText}
-                        />
-
-                        <TouchableOpacity
-                            onPress={handleScreen}
-                            style={styles.signUpContainer}>
-                            <Text style={styles.signUpText}>{isLogin ? "No account?" : "Already have an account?"}</Text>
-                            <Text
-                                style={styles.signUpLink}
-                            >
-                                {isLogin ? "Sign Up" : "Sign In"}
-                            </Text>
-                        </TouchableOpacity>
-=======
                                     {errors?.password && touched?.password &&
                                         <ErrorText
                                             error={errors.password}
@@ -207,11 +146,10 @@ const SignInScreen: React.FC<UserNavigationRootProps<"SignIn">> = (props) => {
                                     style={styles.signUpLink}
                                     onPress={handleScreen}
                                 >
-                                   {isLogin ? "Sign Up " : 'Sign In'}
+                                    {isLogin ? "Sign Up " : 'Sign In'}
                                 </Text>
                             </TouchableOpacity>
                         </Animated.View>
->>>>>>> b965f97f5b292d5a15d63222f22fe369ffb4310d
                     </Animated.View>
                 )
             }}
