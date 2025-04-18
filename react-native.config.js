9ece993d320adaf8a11f020b94de997ac176c30c
amir sohail <74063034+amirsohail0304@users.noreply.github.com>
2025-04-18 15:29:27 +0500

    new pull

--- react-native.config.js
+++ react-native.config.js
@@ -1,40 +1,50 @@
+module.exports = {
+    project: {
+      ios: {},
+      android: {},
+    },
+    assets: ['./src/assets/fonts'],
+  };
+
+
+
 // module.exports = {
 //     assets: ['./assets/fonts/'],
 // };
-module.exports = {
-    // project: {
-    //     android: {
-    //         unstable_reactLegacyComponentNames: [
-    //             'AIRMap',
-    //             'AIRMapCallout',
-    //             'AIRMapCalloutSubview',
-    //             'AIRMapCircle',
-    //             'AIRMapHeatmap',
-    //             'AIRMapLocalTile',
-    //             'AIRMapMarker',
-    //             'AIRMapOverlay',
-    //             'AIRMapPolygon',
-    //             'AIRMapPolyline',
-    //             'AIRMapUrlTile',
-    //             'AIRMapWMSTile',
-    //         ],
-    //     },
-    //     ios: {
-    //         unstable_reactLegacyComponentNames: [
-    //             'AIRMap',
-    //             'AIRMapCallout',
-    //             'AIRMapCalloutSubview',
-    //             'AIRMapCircle',
-    //             'AIRMapHeatmap',
-    //             'AIRMapLocalTile',
-    //             'AIRMapMarker',
-    //             'AIRMapOverlay',
-    //             'AIRMapPolygon',
-    //             'AIRMapPolyline',
-    //             'AIRMapUrlTile',
-    //             'AIRMapWMSTile',
-    //         ],
-    //     },
-    // },
-    assets: ['./assets/fonts/'],
-};
+// module.exports = {
+//     // project: {
+//     //     android: {
+//     //         unstable_reactLegacyComponentNames: [
+//     //             'AIRMap',
+//     //             'AIRMapCallout',
+//     //             'AIRMapCalloutSubview',
+//     //             'AIRMapCircle',
+//     //             'AIRMapHeatmap',
+//     //             'AIRMapLocalTile',
+//     //             'AIRMapMarker',
+//     //             'AIRMapOverlay',
+//     //             'AIRMapPolygon',
+//     //             'AIRMapPolyline',
+//     //             'AIRMapUrlTile',
+//     //             'AIRMapWMSTile',
+//     //         ],
+//     //     },
+//     //     ios: {
+//     //         unstable_reactLegacyComponentNames: [
+//     //             'AIRMap',
+//     //             'AIRMapCallout',
+//     //             'AIRMapCalloutSubview',
+//     //             'AIRMapCircle',
+//     //             'AIRMapHeatmap',
+//     //             'AIRMapLocalTile',
+//     //             'AIRMapMarker',
+//     //             'AIRMapOverlay',
+//     //             'AIRMapPolygon',
+//     //             'AIRMapPolyline',
+//     //             'AIRMapUrlTile',
+//     //             'AIRMapWMSTile',
+//     //         ],
+//     //     },
+//     // },
+//     assets: ['./assets/fonts/'],
+// };
